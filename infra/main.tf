dee4596548524820568ab6b89cff6c17b5b789d2
pedroivoadv <pivo88@gmail.com>
2025-09-03 23:47:38 -0300

    Adicionar configuração do bucket S3 e ajustes de .gitignore

--- infra/main.tf
+++ infra/main.tf
@@ -2,7 +2,7 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "projeto2_bucket" {
-  bucket = "projeto2-deploy-bucket"
-  acl    = "public-read"
+# Criação do bucket privado (padrão)
+resource "aws_s3_bucket" "projeto2-deploy-bucket" {
+  bucket = "projeto2-deploy-bucket" # nome único no mundo
 }
